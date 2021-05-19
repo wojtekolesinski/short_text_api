a044f654654973000292737f307fbfca3c7deaa2
wojtekolesinski <wojtek.olesinski00@gmail.com>
2021-05-19 20:00:50 +0200

    idk

--- short_text_api/short_text_api/settings.py
+++ short_text_api/short_text_api/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qu)$$xhk6f_rqm=s62++)s9%)#r$!1az!m^ydeytba_w#y13$s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
