2b77a8eb15fc81f0f84d352de4003996547f1fc7
wojtekolesinski <wojtek.olesinski00@gmail.com>
2021-05-20 11:42:51 +0200

    registered models for admin

--- short_text_api/api/admin.py
+++ short_text_api/api/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import ShortText
+from django.contrib.auth.models import User
 
 # Register your models here.
+
+
+# @admin.register(User)
+# class UserAdmin(admin.ModelAdmin):
+#     fields = ['username', 'email', 'first_name', 'last_name']
+
+
+@admin.register(ShortText)
+class ShortTextAdmin(admin.ModelAdmin):
+    fields = ['title', 'text', 'viewcount']
--- short_text_api/short_text_api/settings.py
+++ short_text_api/short_text_api/settings.py
@@ -28,7 +28,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 # Application definition
